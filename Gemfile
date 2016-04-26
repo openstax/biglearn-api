a689e6bb517ca7df1fa1f117f1d79eae9f55e0b6
Kevin Burleigh <klb@kindling>
2016-04-25 21:03:15 -0500

    add input/output payload schema validation code

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+## used for input/output payload schema validation
+gem 'json-schema'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,41 @@
+require 'json-schema'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def with_json_apis(input_schema:, output_schema_map:, &block)
+    if request.format != :json
+      payload = { 'errors': [ 'request must have Content-Type = application/json' ] }
+      render json: payload.to_json, status: 400
+      return
+    end
+
+    errors = JSON::Validator.fully_validate(
+      input_schema,
+      request.params[:setup],
+      insert_defaults: true,
+      validate_schema: true
+    )
+    if errors.any?
+      payload = { 'errors': errors }
+      render json: payload.to_json, status: 400
+      return
+    end
+
+    block.call
+
+    unless output_schema_map.has_key? response.status
+      response.status = 500
+      return
+    end
+
+    errors = JSON::Validator.fully_validate(
+      output_schema_map[response.status],
+      JSON.parse(response.body),
+      validate_schema: true
+    )
+    response.status = 500 if errors.any?
+  end
 end
