182ffff09cf7ffa6165e8f2e4b6df6419e29a7d9
Kevin Burleigh <klb@kindling>
2016-04-27 16:42:42 -0500

    change uuid regex to match version4 (totally random) uuid format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -108,7 +108,7 @@ class ApplicationController < ActionController::Base
       'uuid': {
         'type': 'string',
         'pattern': '^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-' +
-                   '[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-'  +
+                   '4[a-fA-F0-9]{3}-[a-fA-F0-9]{4}-'  +
                    '[a-fA-F0-9]{12}$',
       },
       'number_between_0_and_1': {
