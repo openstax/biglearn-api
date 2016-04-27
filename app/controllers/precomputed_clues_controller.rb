0d4d00cd09d126b76fc059a75465205109cf8aa4
Kevin Burleigh <klb@kindling>
2016-04-27 12:43:39 -0500

    add /retrieve_precomputed_clues valid uuid case; refactor

--- app/controllers/precomputed_clues_controller.rb
+++ app/controllers/precomputed_clues_controller.rb
@@ -4,9 +4,42 @@ class PrecomputedCluesController < ApplicationController
     with_json_apis(input_schema:      retrieve_request_payload_schema,
                    output_schema_map: { 200 => retrieve_response_200_payload_schema,
                                         422 => generic_error_schema }) do
-      payload = { 'errors': ['invalid precomputed clue uuid'] }
-      render json: payload.to_json, status: 422
+      errors, precomputed_clues = _process_precomputed_clue_uuids(
+        json_parsed_request_payload['precomputed_clue_uuids']
+      )
+
+      response_status, response_payload =
+        if errors.any?
+          [422, { 'errors': errors }]
+        else
+          [200, { 'precomputed_clues': precomputed_clues }]
+        end
+
+      render json: response_payload.to_json, status: response_status
     end
+  end
+
+  def _process_precomputed_clue_uuids(precomputed_clue_uuids)
+    errors            = []
+    precomputed_clues = []
+
+    valid_precomputed_clue_uuids = [
+      "50fa8fc6-0974-4448-a730-880165a702fe",
+      "5913c263-f91d-4c83-af62-19d4619117f5",
+      "38a89013-57da-4189-8534-d68db933776b",
+      "7d2e17bd-50ff-4f80-a62b-a1f4e1dd6990",
+      "5c04044c-f546-477a-8341-4f32cdc28618"
+    ]
+
+    precomputed_clue_uuids.each_with_index do |uuid, idx|
+      if valid_precomputed_clue_uuids.include? uuid
+        precomputed_clues << "PCP for #{uuid} = #{idx}"
+      else
+        errors << "invalid precomputed_clue_uuid: #{uuid}"
+      end
+    end
+
+    [errors, precomputed_clues]
   end
 
   def retrieve_request_payload_schema
@@ -36,9 +69,7 @@ class PrecomputedCluesController < ApplicationController
       'properties': {
         'precomputed_clues': {
           'type': 'array',
-          'items': {
-            'type': '#definitions/clue',
-          },
+          'items': {'$ref': '#definitions/clue'},
         },
       },
       'required': ['precomputed_clues'],
--- spec/api_integration/retrieve_precomputed_clue_with_invalid_uuid_spec.rb
+++ spec/api_integration/retrieve_precomputed_clue_with_invalid_uuid_spec.rb
@@ -2,18 +2,48 @@ require 'rails_helper'
 
 describe 'POST /retrieve_precomputed_clues' do
 
-  context 'with invalid uuid' do
-    it 'returns 422 (unprocessable entity)', type: :request do
-      payload = { 'precomputed_clue_uuids': [ SecureRandom::uuid.to_s ] }
+  context 'with invalid uuid(s)' do
+    it 'returns 422 (unprocessable entity) with appropriate error messages', type: :request do
+      target_invalid_uuids = [ SecureRandom.uuid.to_s, SecureRandom.uuid.to_s ]
 
-      make_post_request(
-        route: '/retrieve_precomputed_clues',
-        headers: { 'Content-Type' => 'application/json' },
-        body:  payload.to_json
-      )
-      # debugger
-      expect(response.status).to eq(422)
+      response_status, response_payload = request_precomputed_clues(target_invalid_uuids)
+
+      expect(response_status).to eq(422)
+      target_invalid_uuids.each do |target_invalid_uuid|
+        expect(response_payload['errors'].grep(/#{target_invalid_uuid}/)).to_not be_empty
+      end
     end
   end
 
+  context 'with valid uuid(s)' do
+    it 'returns 200 (success) with appropriate number of precomputed CLUEs', type: :request do
+      ## these are drawn from the currently hard-coded
+      ## valid uuids in the controller
+      target_valid_uuids = [
+        "5913c263-f91d-4c83-af62-19d4619117f5",
+        "38a89013-57da-4189-8534-d68db933776b",
+        "7d2e17bd-50ff-4f80-a62b-a1f4e1dd6990"
+      ]
+
+      response_status, response_payload = request_precomputed_clues(target_valid_uuids)
+
+      expect(response_status).to eq(200)
+      expect(response_payload['precomputed_clues'].count).to eq(target_valid_uuids.count)
+    end
+  end
+
+end
+
+def request_precomputed_clues(precomputed_clue_uuids)
+  request_payload = { 'precomputed_clue_uuids': precomputed_clue_uuids }
+
+  make_post_request(
+    route: '/retrieve_precomputed_clues',
+    headers: { 'Content-Type' => 'application/json' },
+    body:  request_payload.to_json
+  )
+  response_payload = JSON.parse(response.body)
+  response_status = response.status
+
+  [response_status, response_payload]
 end
