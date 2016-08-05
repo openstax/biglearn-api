9c3f54233f4c130bff2d60574292b1eb10f3b7c4
Kevin Burleigh <klb@kindling>
2016-08-05 18:16:50 -0500

    cleanup question pool scenarios

--- spec/api_integration/question_pool_scenarios_spec.rb
+++ spec/api_integration/question_pool_scenarios_spec.rb
@@ -1,23 +1,30 @@
 require 'rails_helper'
 
 describe 'question pool scenarios' do
-  context 'creating question pool using invalid question uuids' do
-    it 'returns 422 (unprocessable entity) with appropriate error messages(s)', type: :request do
-      invalid_question_uuids = 10.times.collect{ SecureRandom.uuid.to_s }
-      question_pool_defs = [ { question_uuids: invalid_question_uuids[0..5]  },
-                            { question_uuids: invalid_question_uuids[6..-1] } ]
+  context 'creating question pool using invalid question uuids', type: :request do
+    before(:each) do
+      @invalid_question_uuids = 10.times.collect{ SecureRandom.uuid.to_s }
+      question_pool_defs = [ { question_uuids: @invalid_question_uuids[0..5]  },
+                            { question_uuids: @invalid_question_uuids[6..-1] } ]
 
-      response_status, response_payload = create_question_pools(question_pool_defs)
+      @response_status, @response_payload = create_question_pools(question_pool_defs)
+    end
+    let(:invalid_question_uuids) { @invalid_question_uuids }
+    let(:response_status)        { @response_status }
+    let(:response_payload)       { @response_payload }
 
+    it 'returns status 422 (unprocessable entity)' do
       expect(response_status).to eq(422)
+    end
+    it 'returns the appropriate error messages(s)' do
       invalid_question_uuids.each do |uuid|
         expect(response_payload['errors'].grep(/#{uuid}/)).to_not be_empty
       end
     end
   end
 
-  context 'creating question pool using valid question uuids' do
-    it 'returns 200 (success) with appropriate number of question uuids', type: :request do
+  context 'creating question pool using valid question uuids', type: :request do
+    before(:each) do
       ##
       ## Create question uuids
       ##
@@ -35,9 +42,15 @@ describe 'question pool scenarios' do
       question_pool_defs = [ { question_uuids: question_uuids[0..3] },
                             { question_uuids: question_uuids[3..4] } ]
 
-      response_status, response_payload = create_question_pools(question_pool_defs)
+      @response_status, @response_payload = create_question_pools(question_pool_defs)
+    end
+    let(:response_status)  { @response_status }
+    let(:response_payload) { @response_payload }
 
+    it 'returns status 200 (success)' do
       expect(response_status).to eq(200)
+    end
+    it 'returns the appropriate number of question uuids' do
       expect(response_payload['question_pool_uuids'].count).to eq(2)
     end
   end
