c621a33689842f2fe4536ca95df529428d067a95
Kevin Burleigh <klb@kindling>
2016-08-09 07:23:48 -0500

    simplified protocol logic

--- lib/protocol.rb
+++ lib/protocol.rb
@@ -1,4 +1,5 @@
 class Protocol
+
   def initialize(protocol_name:, min_work_interval:, group_uuid:, &block)
     @protocol_name      = protocol_name
     @min_work_interval  = min_work_interval
@@ -7,76 +8,67 @@ class Protocol
     @instance_uuid      = SecureRandom.uuid.to_s
   end
 
+
   def run
     ActiveRecord::Base.clear_active_connections!
 
     last_work_time = Time.now - 1.year
 
     loop do
-      my_record, group_records = _read_records
+      my_record, group_records, dead_records = _read_records
       unless my_record
+        puts "create!"
         _create_record
         next
       end
 
       am_boss, boss_record = _get_boss_situation(group_records)
-
-      my_record.boss_uuid = boss_record ? boss_record.instance_uuid : my_record.instance_uuid
-      _save_record(my_record)
-
-      if !boss_record
-        puts 'election!'
-        _elect_new_boss
+      unless boss_record
+        puts "elect!"
+        _elect_new_boss(my_record, group_records)
         next
       end
 
+      my_record.boss_uuid = boss_record.instance_uuid
       if am_boss
         my_record.boss_instance_count = group_records.count
-        _save_record(my_record)
+      else
+        my_record.boss_instance_count = -1
+      end
+      _save_record(my_record)
 
-        dead_records = _read_dead_records
-        dead_records.map(&:destroy!)
-
-        actual_modulos = group_records.map(&:instance_modulo).sort
-        target_modulos = (0..group_records.count-1).to_a
-        if actual_modulos != target_modulos
-          my_record.boss_command = 'allocate_modulos'
-          _save_record(my_record)
-        else
-          my_record.boss_command = 'work'
-          _save_record(my_record)
-        end
+      if am_boss && dead_records.any?
+        puts "destroy!"
+        dead_records.map(&:destroy)
+        next
       end
 
-
-      am_boss, boss_record = _get_boss_situation(group_records)
-      next unless boss_record
-
-      case boss_record.boss_command
-      # when 'clear_modulos'
-      #   puts 'clear_modulos!'
-      #   _clear_modulos
-      when 'allocate_modulos'
-        puts 'allocate_modulos!'
-        _allocate_modulos
-      when 'work'
-        curent_time = Time.now
-        if my_record.instance_modulo < 0
-          sleep(0.5)
-        elsif curent_time - last_work_time >= @min_work_interval
-          last_work_time = curent_time
-          # puts "#{my_record.instance_uuid} #{my_record.instance_modulo} #{am_boss} #{boss_record.instance_uuid} - WORK"
-          # sleep(0.1)
-          @work_block.call(
-            instance_count:  boss_record.boss_instance_count,
-            instance_modulo: my_record.instance_modulo,
-          )
-        else
-          sleep([0.5, (last_work_time + @min_work_interval - curent_time)].min)
+      actual_modulos = group_records.map(&:instance_modulo).sort
+      target_modulos = (0..boss_record.boss_instance_count-1).to_a
+      if actual_modulos != target_modulos
+        puts "allocate needed!"
+        if (my_record.instance_modulo < 0) || (my_record.instance_modulo >= boss_record.boss_instance_count)
+          puts "allocate myself!"
+          _allocate_modulo(my_record, group_records)
         end
+        sleep(0.1)
+        next
+      end
+
+      if (my_record.instance_modulo < 0) || (my_record.instance_modulo >= boss_record.boss_instance_count)
+        raise "instance_modulo error (#{my_record.instance_modulo} / #{boss_record.boss_instance_count})"
+      end
+
+      # puts "work!"
+      curent_time = Time.now
+      if curent_time - last_work_time >= @min_work_interval
+        last_work_time = curent_time
+        @work_block.call(
+          instance_count:  boss_record.boss_instance_count,
+          instance_modulo: my_record.instance_modulo,
+        )
       else
-        puts "#{my_record.instance_uuid} #{my_record.instance_modulo} #{am_boss} #{boss_record.instance_uuid} ..."
-        sleep(1)
+        sleep([0.5, (last_work_time + @min_work_interval - curent_time)].min)
       end
     end
   rescue Interrupt => ex
@@ -87,7 +79,10 @@ class Protocol
     _destroy_record
   end
 
+
   def _create_record
+    puts "create!"
+
     loop do
       retries ||= 0
 
@@ -99,7 +94,7 @@ class Protocol
             protocol_name:       @protocol_name,
             group_uuid:          @group_uuid,
             instance_uuid:       @instance_uuid,
-            boss_uuid:           SecureRandom.uuid.to_s,
+            boss_uuid:           @instance_uuid,
             boss_command:        'none',
             boss_instance_count:  -1,
             instance_command:    'none',
@@ -116,12 +111,14 @@ class Protocol
     end
   end
 
+
   def _save_record(record)
     ActiveRecord::Base.connection_pool.with_connection do
       record.touch
       record.save!
     end
   end
+
 
   def _destroy_record
     my_record = ActiveRecord::Base.connection_pool.with_connection do
@@ -130,22 +127,19 @@ class Protocol
     my_record.destroy! if my_record
   end
 
+
   def _read_records
-    group_records = ActiveRecord::Base.connection_pool.with_connection do
-      ProtocolRecord.where{group_uuid == my{@group_uuid}}
-                    .where{updated_at > Time.now - 10.seconds}.to_a
+    all_records = ActiveRecord::Base.connection_pool.with_connection do
+      ProtocolRecord.where{group_uuid == my{@group_uuid}}.to_a
     end
-    my_record = group_records.detect{|rec| rec.instance_uuid == @instance_uuid}
-    [my_record, group_records]
+
+    group_records = all_records.select{|rec| rec.updated_at > Time.now - 10.seconds}
+    dead_records  = all_records - group_records
+    my_record     = all_records.detect{|rec| rec.instance_uuid == @instance_uuid}
+
+    [my_record, group_records, dead_records]
   end
 
-  def _read_dead_records
-    dead_records = ActiveRecord::Base.connection_pool.with_connection do
-      ProtocolRecord.where{group_uuid == my{@group_uuid}}
-                    .where{updated_at <= Time.now - 10.seconds}.to_a
-    end
-    dead_records
-  end
 
   def _get_boss_situation(group_records)
     uuid, votes = group_records.group_by(&:boss_uuid)
@@ -154,6 +148,9 @@ class Protocol
                                   result
                                }.sort_by{|uuid, size| size}
                                .last
+
+    # puts "uuid: #{uuid} votes: #{votes} count: #{group_records.count}"
+
     boss_uuid = (votes > group_records.count/2.0) ? uuid : nil
     boss_uuid = nil unless group_records.detect{|rec| rec.instance_uuid == boss_uuid}
 
@@ -164,126 +161,51 @@ class Protocol
   end
 
 
-  def _elect_new_boss
-    my_record, group_records = _read_records
-
-    am_boss, boss_record = _get_boss_situation(group_records)
-    return if boss_record
-
+  def _elect_new_boss(my_record, group_records)
     lowest_uuid = group_records.map(&:instance_uuid).sort.first
 
-    my_record.boss_uuid        = lowest_uuid
-    my_record.boss_command     = 'none'
-    my_record.instance_command = 'elect'
+    my_record.boss_uuid           = lowest_uuid
+    my_record.boss_command        = 'none'
+    my_record.boss_instance_count = group_records.count
+    my_record.instance_command    = 'elect'
 
     _save_record(my_record)
-
-    loop do
-      start_time ||= Time.now
-
-      my_record, group_records = _read_records
-      am_boss, boss_record = _get_boss_situation(group_records)
-
-      break if boss_record
-      raise 'election failed' if start_time < Time.now - 10.seconds
-
-      sleep(0.1)
-    end
+    sleep(0.1)
   end
 
-  def _allocate_modulos
-    loop do
-      start_time ||= Time.now
 
-      my_record, group_records = _read_records
-      am_boss, boss_record = _get_boss_situation(group_records)
-      return if !boss_record
+  def _allocate_modulo(my_record, group_records)
+    am_boss, boss_record = _get_boss_situation(group_records)
+    return if !boss_record
 
+    boss_instance_count = boss_record.boss_instance_count
+
+    all_modulos = (0..boss_instance_count-1).to_a
+    taken_modulos = group_records.select{ |rec|
+      (rec.instance_modulo >= 0) && (rec.instance_modulo < boss_instance_count)
+    }.map(&:instance_modulo).sort
+
+    available_modulos = all_modulos - taken_modulos
+
+    puts "boss_instance_count = #{boss_instance_count}"
+    puts "all_modulos:       #{all_modulos}"
+    puts "taken_modulos:     #{taken_modulos}"
+    puts "available_modulos: #{available_modulos}"
+
+    available_modulos.each do |target_modulo|
+      puts "target_modulo = #{target_modulo}"
       begin
         my_record.instance_command = 'allocate_modulos'
-        my_record.instance_modulo  = -1000 - rand(1000)
+        my_record.instance_modulo  = target_modulo
 
         _save_record(my_record)
+        break
       rescue ActiveRecord::WrappedDatabaseException
         sleep(0.1)
-        next
       end
-
-      my_record, group_records = _read_records
-      am_boss, boss_record = _get_boss_situation(group_records)
-      return if !boss_record
-
-      boss_instance_count = boss_record.boss_instance_count
-
-      success = false
-      boss_instance_count.times do |target_modulo|
-        puts "target_modulo = #{target_modulo}"
-        begin
-          my_record, group_records = _read_records
-
-          my_record.instance_command = 'allocate_modulos'
-          my_record.instance_modulo  = target_modulo
-
-          _save_record(my_record)
-          success = true
-        rescue ActiveRecord::WrappedDatabaseException
-          sleep(0.1)
-          next
-        end
-      end
-
-      break if success
-      raise "could not allocate modulos" if start_time < Time.now - 10.seconds
-      sleep(0.1)
     end
 
-    _wait_for_boss
+    sleep(0.1)
   end
 
-  def _wait_for_boss
-    loop do
-      my_record, group_records = _read_records
-      am_boss, boss_record = _get_boss_situation(group_records)
-      return if !boss_record
-
-      my_record.instance_command = 'wait_for_boss'
-      _save_record(my_record)
-
-      if am_boss
-        if group_records.all?{|rec| rec.instance_command == 'wait_for_boss'}
-          my_record.boss_command     = 'none'
-          my_record.instance_command = 'none'
-          _save_record(my_record)
-          break
-        end
-      else
-        if boss_record.boss_command == 'none'
-          my_record.instance_command = 'none'
-          _save_record(my_record)
-          break
-        end
-      end
-
-      sleep(0.1)
-    end
-
-    loop do
-      my_record, group_records = _read_records
-      am_boss, boss_record = _get_boss_situation(group_records)
-      return if !boss_record || !am_boss
-
-      break if group_records.all?{|rec| rec.instance_command == 'none'}
-      sleep(0.1)
-    end
-  end
-
-  def self.at_most_every(duration, &block)
-    loop do
-      t1 = Time.now
-      block.call(instance_count: 2, instance_modulo: 1)
-      t2 = Time.now
-      elapsed = t2 - t1
-      sleep(duration - elapsed) if duration > elapsed
-    end
-  end
 end
