c56a824a73cac9fd331c6a0090aae909c322ddc3
pandafulmanda <pandafulmanda@gmail.com>
2016-10-03 15:34:12 -0500

    remove input validation from service, check for course existence before inserting

--- app/domain/services/create_course/service.rb
+++ app/domain/services/create_course/service.rb
@@ -1,6 +1,5 @@
 class Services::CreateCourse::Service
   def process(course_uuid:, ecosystem_uuid:)
-    return nil if course_uuid.nil? or ecosystem_uuid.nil?
 
     start_time     = Time.now
     start_time_str = start_time.utc.iso8601(6)
@@ -11,24 +10,31 @@ class Services::CreateCourse::Service
       if ecosystem.nil?
 
     values_str = %Q{
-      ( '#{course_uuid}',
+      '#{course_uuid}',
         '#{ecosystem_uuid}',
         TIMESTAMP WITH TIME ZONE '#{start_time_str}',
-        TIMESTAMP WITH TIME ZONE '#{start_time_str}' )
+        TIMESTAMP WITH TIME ZONE '#{start_time_str}'
+    }.gsub(/\n\s*/, ' ')
+
+    select_str = %Q{
+      SELECT 1
+        FROM courses
+        WHERE
+          courses.uuid = '#{course_uuid}'
     }.gsub(/\n\s*/, ' ')
 
     created_course_uuid = Course.transaction(isolation: :serializable) do
       sql_inserted_course = %Q{
         INSERT INTO courses
-        (uuid,ecosystem_uuid,created_at,updated_at)
-        VALUES #{values_str}
-        ON CONFLICT DO NOTHING
+        (uuid, ecosystem_uuid, created_at, updated_at)
+        SELECT #{values_str}
+        WHERE NOT EXISTS (#{select_str})
         RETURNING uuid
       }.gsub(/\n\s*/, ' ')
 
-      inserted_course = Course.connection.execute(sql_inserted_course)
+      inserted_courses = Course.connection.execute(sql_inserted_course)
 
-      inserted_course.collect{|hash| hash.fetch('uuid')}[0]
+      inserted_courses.collect{|hash| hash.fetch('uuid')}[0] or course_uuid
     end
 
     {
--- spec/app/domain/services/create_course/service_spec.rb
+++ spec/app/domain/services/create_course/service_spec.rb
@@ -2,67 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Services::CreateCourse::Service do
   let(:service) { Services::CreateCourse::Service.new }
+  let(:action) { process }
 
-  let(:action) { service.process(course_uuid: given_course_uuid, ecosystem_uuid: given_ecosystem_uuid) }
-
-  context "when no data is given" do
-    let(:given_course_uuid) { nil }
-    let(:given_ecosystem_uuid) { nil }
-
-    let!(:split_time) { time = Time.now; sleep(0.001); time }
-
-    it "no Courses are created" do
-      expect{action}.to_not change{Course.count}
-    end
-    it "no Courses are updated" do
-      action
-
-      updated_courses = Course.where{updated_at > my{split_time}}
-      expect(updated_courses).to be_empty
-    end
-    it "nil is returned" do
-      expect(action).to be_nil
-    end
-  end
-
-  context "when course_uuid is missing" do
-    let(:given_course_uuid) { nil }
-    let(:given_ecosystem_uuid) { SecureRandom.uuid.to_s }
-
-    let!(:split_time) { time = Time.now; sleep(0.001); time }
-
-    it "no Courses are created" do
-      expect{action}.to_not change{Course.count}
-    end
-    it "no Courses are updated" do
-      action
-
-      updated_courses = Course.where{updated_at > my{split_time}}
-      expect(updated_courses).to be_empty
-    end
-    it "nil is returned" do
-      expect(action).to be_nil
-    end
-  end
-
-  context "when ecosystem_uuid is missing" do
-    let(:given_course_uuid) { SecureRandom.uuid.to_s }
-    let(:given_ecosystem_uuid) { nil }
-
-    let!(:split_time) { time = Time.now; sleep(0.001); time }
-
-    it "no Courses are created" do
-      expect{action}.to_not change{Course.count}
-    end
-    it "no Courses are updated" do
-      action
-
-      updated_courses = Course.where{updated_at > my{split_time}}
-      expect(updated_courses).to be_empty
-    end
-    it "nil is returned" do
-      expect(action).to be_nil
-    end
+  def process
+    service.process(course_uuid: given_course_uuid, ecosystem_uuid: given_ecosystem_uuid)
   end
 
   context "when non-existing ecosystem is given" do
@@ -99,12 +42,40 @@ RSpec.describe Services::CreateCourse::Service do
     it "created_course_uuid is returned" do
       expect(action).to eq expected
     end
+
     it "Course is updated" do
       action
 
       updated_courses = Course.where{updated_at > my{split_time}}
       expect(updated_courses).to_not be_empty
     end
+  end
+
+  context "when existing course is given" do
+
+    ecosystem_uuid = SecureRandom.uuid.to_s
+    course_uuid = SecureRandom.uuid.to_s
+
+    let(:given_course_uuid) { course_uuid }
+    let(:given_ecosystem_uuid) { ecosystem_uuid }
+
+    expected = {created_course_uuid: course_uuid}
+
+    let!(:split_time) { time = Time.now; sleep(0.001); time }
+
+    before(:each) do
+      Ecosystem.find_or_create_by(uuid: ecosystem_uuid)
+      process
+    end
+
+    it "Course is not created" do
+      expect{process}.to_not change{Course.count}
+    end
+
+    it "created_course_uuid is returned" do
+      expect(process).to eq expected
+    end
+
   end
 
 end
