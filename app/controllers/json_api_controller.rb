9d948682c5f452e4e3a31edcacab309c0a184959
Nathan Stitt <nathan@stitt.org>
2016-10-14 16:48:46 -0500

    make definitions available on class & instance

--- app/controllers/json_api_controller.rb
+++ app/controllers/json_api_controller.rb
@@ -35,7 +35,6 @@ class JsonApiController < ApplicationController
       insert_defaults: true,
       validate_schema: true
     )
-
     fail Errors::AppRequestSchemaError.new('request body failed validation', validation_errors) \
       if validation_errors.any?
   end
@@ -100,74 +99,82 @@ class JsonApiController < ApplicationController
   end
 
 
-  def _standard_definitions
-    {
-      'uuid': {
-        'type': 'string',
-        'pattern': '^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-' +
-                   '4[a-fA-F0-9]{3}-[a-fA-F0-9]{4}-' +
-                   '[a-fA-F0-9]{12}$',
-      },
-      'number_between_0_and_1': {
-        'type': 'number',
-        'minimum': 0,
-        'maximum': 1,
-      },
-      'non_negative_integer': {
-        'type': 'integer',
-        'minumum': 0,
-      },
-      'datetime': {
-        'type': 'string',
-        'pattern': '^\d{4}-'                       + ## year
-                   '(0[1-9]|1[0-2])-'              + ## month
-                   '(0[1-9]|1[0-9]|2[0-9]|3[0-1])' + ## day of month
-                   '(T|t)'                         + ## ISO8601 spacer
-                   '(0[0-9]|1[0-9]|2[0-3]):'       + ## hour
-                   '([0-5][0-9]):'                 + ## minute
-                   '([0-5][0-9]|60)'               + ## second
-                   '\.\d{6}'                       + ## fraction of second
-                   '(Z|z)$'                          ## Zulu timezone
-      },
-      'receiver_info': {
-        'type': 'object',
-        'properties': {
-          'receiver_uuid': {'$ref': '#/standard_definitions/uuid'},
-          'partition_count': {
-            'type': 'integer',
-            'minumum': 0,
-            'maximum': 10000,
-          },
-          'partition_modulo': {
-            'type': 'integer',
-            'minumum': 0,
-            'maximum': 9999,
-          },
-        },
-        'required': ['receiver_uuid', 'partition_count', 'partition_modulo'],
-        'additionalProperties': false,
-      },
-    }
+  module SchemaDefinitions
+
+    def _standard_definitions
+      {
+        'uuid': {
+                  'type': 'string',
+                 'pattern': '^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-' +
+                            '4[a-fA-F0-9]{3}-[a-fA-F0-9]{4}-' +
+                            '[a-fA-F0-9]{12}$',
+                },
+       'number_between_0_and_1': {
+                                   'type': 'number',
+                                  'minimum': 0,
+                                  'maximum': 1,
+                                 },
+       'non_negative_integer': {
+                                 'type': 'integer',
+                                'minumum': 0,
+                               },
+       'datetime': {
+                     'type': 'string',
+                    'pattern': '^\d{4}-'                       + ## year
+                               '(0[1-9]|1[0-2])-'              + ## month
+                               '(0[1-9]|1[0-9]|2[0-9]|3[0-1])' + ## day of month
+                               '(T|t)'                         + ## ISO8601 spacer
+                               '(0[0-9]|1[0-9]|2[0-3]):'       + ## hour
+                               '([0-5][0-9]):'                 + ## minute
+                               '([0-5][0-9]|60)'               + ## second
+                               '\.\d{6}'                       + ## fraction of second
+                               '(Z|z)$'                          ## Zulu timezone
+                   },
+       'receiver_info': {
+                          'type': 'object',
+                         'properties': {
+                                         'receiver_uuid': {'$ref': '#/standard_definitions/uuid'},
+                                        'partition_count': {
+                                                             'type': 'integer',
+                                                            'minumum': 0,
+                                                            'maximum': 10000,
+                                                           },
+                                        'partition_modulo': {
+                                                              'type': 'integer',
+                                                             'minumum': 0,
+                                                             'maximum': 9999,
+                                                            },
+                                       },
+                         'required': ['receiver_uuid', 'partition_count', 'partition_modulo'],
+                         'additionalProperties': false,
+                        },
+      }
+    end
+
+
+    def _generic_error_schema
+      {
+        '$schema': JSON_SCHEMA,
+
+       'type': 'object',
+       'properties': {
+                       'errors': {
+                                   'type': 'array',
+                                  'items': {
+                                             'type': 'string',
+                                           },
+                                  'minItems': 1,
+                                 },
+                     },
+       'required': ['errors'],
+       'additionalProperties': false,
+      }
+    end
+
+
   end
-
-
-  def _generic_error_schema
-    {
-      '$schema': JSON_SCHEMA,
-
-      'type': 'object',
-      'properties': {
-        'errors': {
-          'type': 'array',
-          'items': {
-            'type': 'string',
-          },
-          'minItems': 1,
-        },
-      },
-      'required': ['errors'],
-      'additionalProperties': false,
-    }
-  end
+  # make methods available both on instance and class
+  include SchemaDefinitions
+  extend SchemaDefinitions
 
 end
