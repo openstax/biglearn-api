3205e9eb4d0e7b0daf1d9fecb964c5bb54b421cc
Dante Soares <dante.m.soares@rice.edu>
2017-04-11 14:46:42 -0500

    Add new_framework_defaults initializer and new boot.rb

--- app/models/student_pe.rb
+++ app/models/student_pe.rb
@@ -1,8 +1,4 @@
 class StudentPe < ApplicationRecord
-  belongs_to :student, primary_key: :uuid,
-                       foreign_key: :student_uuid,
-                       inverse_of: :student_pe
-
   validates :student_uuid,   presence: true
   validates :algorithm_name, presence: true, uniqueness: { scope: :student_uuid }
 end
