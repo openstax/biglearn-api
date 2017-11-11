55b288130004624eed5cf7b50c1f4e8b0c7371cb
Dante Soares <dante.m.soares@rice.edu>
2017-11-10 19:15:21 -0600

    Let PostgreSQL limit the size of returned events using window functions instead of relying on COPY TO STDOUT

--- app/domain/services/fetch_course_events/service.rb
+++ app/domain/services/fetch_course_events/service.rb
@@ -1,6 +1,6 @@
 # This code will not return events with gaps in the sequence_number
 class Services::FetchCourseEvents::Service < Services::ApplicationService
-  MAX_DATA_SIZE = 1000000 # For the data field, in characters
+  MAX_DATA_SIZE = 3.2e7 # For the data field, in bits
 
   def process(course_event_requests:, max_num_events:)
     return { course_event_responses: [] } if course_event_requests.empty?
@@ -19,58 +19,58 @@ class Services::FetchCourseEvents::Service < Services::ApplicationService
         ON "course_events"."course_uuid" = "requests"."course_uuid"
           AND "course_events"."sequence_number" >= "requests"."sequence_number_offset"
           AND "course_events"."type" = ANY ("requests"."event_types")
+      WINDOW "window" AS (
+        ORDER BY "course_events"."sequence_number" - "requests"."sequence_number_offset"
+          ASC NULLS FIRST
+        ROWS UNBOUNDED PRECEDING
+      )
     JOIN_SQL
 
     # Also return gap information about each record
-    course_event_sql = CourseEvent.select(
-      <<-SQL.strip_heredoc
-        "course_events"."sequence_number",
-        "course_events"."uuid",
-        "course_events"."type",
-        "course_events"."data",
-        CASE WHEN "course_events"."sequence_number" > 0
-          AND NOT EXISTS (
-            SELECT "previous_event".*
-            FROM "course_events" AS "previous_event"
-            WHERE "previous_event"."course_uuid" = "course_events"."course_uuid"
-              AND "previous_event"."sequence_number" = "course_events"."sequence_number" - 1
-          ) THEN TRUE
-        ELSE FALSE
-        END AS "is_after_gap",
-        CASE WHEN NOT EXISTS (
-          SELECT "next_events".*
-          FROM "course_events" AS "next_events"
-          WHERE "next_events"."course_uuid" = "course_events"."course_uuid"
-            AND "next_events"."sequence_number" > "course_events"."sequence_number"
-            AND "next_events"."type" = ANY ("requests"."event_types")
-        ) THEN TRUE
-        ELSE FALSE
-        END AS "is_end",
-        "requests"."request_uuid"
-      SQL
-    )
-    .joins(course_event_join_query)
-    .order(
-      '"course_events"."sequence_number" - "requests"."sequence_number_offset" ASC NULLS FIRST'
-    )
-    .limit(max_num_events)
-    .to_sql
-
-    # Stream the data from Postgres and stop when the size limit is exceeded
-    connection = CourseEvent.connection.raw_connection
-    decoder = PG::TextDecoder::CopyRow.new
-    total_data_size = 0
-    rows_by_request_uuid = Hash.new { |hash, key| hash[key] = [] }
-    connection.copy_data "COPY (#{course_event_sql}) TO STDOUT", decoder do
-      while row = connection.get_copy_data
-        data_size = row.fourth.nil? ? 0 : row.fourth.size
-        next if data_size > 0 && total_data_size >= MAX_DATA_SIZE
-
-        total_data_size += data_size
-        request_uuid = row[6]
-        rows_by_request_uuid[request_uuid] << row
-      end
-    end
+    course_events = CourseEvent
+      .from("(#{
+        CourseEvent.select(
+          <<-SELECT_SQL.strip_heredoc
+            "course_events"."sequence_number",
+            "course_events"."uuid",
+            "course_events"."type",
+            "course_events"."data",
+            ROW_NUMBER() OVER "window" AS "row_number",
+            SUM(BIT_LENGTH("course_events"."data"::text)) OVER "window" AS "cumulative_size",
+            CASE WHEN "course_events"."sequence_number" > 0
+              AND NOT EXISTS (
+                SELECT "previous_event".*
+                FROM "course_events" AS "previous_event"
+                WHERE "previous_event"."course_uuid" = "course_events"."course_uuid"
+                  AND "previous_event"."sequence_number" = "course_events"."sequence_number" - 1
+              ) THEN TRUE
+            ELSE FALSE
+            END AS "is_after_gap",
+            CASE WHEN NOT EXISTS (
+              SELECT "next_events".*
+              FROM "course_events" AS "next_events"
+              WHERE "next_events"."course_uuid" = "course_events"."course_uuid"
+                AND "next_events"."sequence_number" > "course_events"."sequence_number"
+                AND "next_events"."type" = ANY ("requests"."event_types")
+            ) THEN TRUE
+            ELSE FALSE
+            END AS "is_end",
+            "requests"."request_uuid"
+          SELECT_SQL
+        )
+        .joins(course_event_join_query)
+        .order(
+          '"course_events"."sequence_number" - "requests"."sequence_number_offset" ASC NULLS FIRST'
+        )
+        .limit(max_num_events)
+        .to_sql
+      }) AS \"course_events\"")
+      .where(
+        <<-WHERE_SQL.strip_heredoc
+          "row_number" = 1 OR "cumulative_size" IS NULL OR "cumulative_size" < #{MAX_DATA_SIZE.to_i}
+        WHERE_SQL
+      )
+    course_events_by_request_uuid = course_events.group_by(&:request_uuid)
 
     responses = course_event_requests.map do |request|
       request_uuid = request.fetch(:request_uuid)
@@ -83,20 +83,22 @@ class Services::FetchCourseEvents::Service < Services::ApplicationService
         events: [],
         is_gap: false,
         is_end: false
-      } if !rows_by_request_uuid.has_key?(request_uuid)
+      } if !course_events_by_request_uuid.has_key?(request_uuid)
 
       is_gap = false
-      rows = rows_by_request_uuid[request_uuid]
-      event_hashes = rows.map do |row|
-        is_gap = true if row.fifth == 't'
+      course_events = course_events_by_request_uuid[request_uuid]
+      event_hashes = course_events.map do |course_event|
+        is_gap = true if course_event.is_after_gap
+        next if is_gap
 
-        next if is_gap || row.fourth.nil?
+        uuid = course_event.uuid
+        next if uuid.nil?
 
         {
-          sequence_number: row.first.to_i,
-          event_uuid: row.second,
-          event_type: CourseEvent.types.key(row.third.to_i),
-          event_data: JSON.parse(row.fourth)
+          sequence_number: course_event.sequence_number,
+          event_uuid: uuid,
+          event_type: course_event.type,
+          event_data: course_event.data
         }
       end.compact
 
@@ -105,7 +107,7 @@ class Services::FetchCourseEvents::Service < Services::ApplicationService
         course_uuid: course_uuid,
         events: event_hashes,
         is_gap: is_gap,
-        is_end: !is_gap && rows.last[5] == 't'
+        is_end: !is_gap && course_events.last.is_end
       }
     end
 
