a18d608e5367bc76fd0c558c34f95c56ea922e9b
Dante Soares <dante.m.soares@rice.edu>
2019-10-28 20:07:24 -0500

    Turned part of the latest migration into a background migration

--- db/migrate/20190930214433_add_calculation_uuid_and_ecosystem_matrix_uuid_to_exercises_and_clues.rb
+++ db/migrate/20190930214433_add_calculation_uuid_and_ecosystem_matrix_uuid_to_exercises_and_clues.rb
@@ -1,37 +1,61 @@
 class AddCalculationUuidAndEcosystemMatrixUuidToExercisesAndClues < ActiveRecord::Migration[5.2]
   NIL_UUID = '00000000-0000-0000-0000-000000000000'
 
-  def change
-    add_column :assignment_pes, :calculation_uuid, :uuid
-    change_column_null :assignment_pes, :calculation_uuid, false, NIL_UUID
-    add_index :assignment_pes, :calculation_uuid
+  disable_ddl_transaction!
 
-    add_column :assignment_pes, :ecosystem_matrix_uuid, :uuid
-    change_column_null :assignment_pes, :ecosystem_matrix_uuid, false, NIL_UUID
-    add_index :assignment_pes, :ecosystem_matrix_uuid
+  def up
+    ActiveRecord::Base.transaction do
+      add_column :assignment_pes, :calculation_uuid, :uuid
+      add_column :assignment_pes, :ecosystem_matrix_uuid, :uuid
+      add_column :assignment_spes, :calculation_uuid, :uuid
+      add_column :assignment_spes, :ecosystem_matrix_uuid, :uuid
+      add_column :student_pes, :calculation_uuid, :uuid
+      add_column :student_pes, :ecosystem_matrix_uuid, :uuid
+      add_column :student_clues, :calculation_uuid, :uuid
+      add_column :teacher_clues, :calculation_uuid, :uuid
+    end
 
-    add_column :assignment_spes, :calculation_uuid, :uuid
-    change_column_null :assignment_spes, :calculation_uuid, false, NIL_UUID
-    add_index :assignment_spes, :calculation_uuid
+    # A simple background migration with the drawback that it will not retry errors
+    # This code will run after all migrations have finished
+    background_migration = -> do
+      # Become a daemon so the calling process can exit successfully
+      Process.daemon
 
-    add_column :assignment_spes, :ecosystem_matrix_uuid, :uuid
-    change_column_null :assignment_spes, :ecosystem_matrix_uuid, false, NIL_UUID
-    add_index :assignment_spes, :ecosystem_matrix_uuid
+      change_column_null :assignment_pes, :calculation_uuid, false, NIL_UUID
+      change_column_null :assignment_pes, :ecosystem_matrix_uuid, false, NIL_UUID
+      change_column_null :assignment_spes, :calculation_uuid, false, NIL_UUID
+      change_column_null :assignment_spes, :ecosystem_matrix_uuid, false, NIL_UUID
+      change_column_null :student_pes, :calculation_uuid, false, NIL_UUID
+      change_column_null :student_pes, :ecosystem_matrix_uuid, false, NIL_UUID
+      change_column_null :student_clues, :calculation_uuid, false, NIL_UUID
+      change_column_null :teacher_clues, :calculation_uuid, false, NIL_UUID
 
-    add_column :student_pes, :calculation_uuid, :uuid
-    change_column_null :student_pes, :calculation_uuid, false, NIL_UUID
-    add_index :student_pes, :calculation_uuid
+      add_index :assignment_pes, :calculation_uuid
+      add_index :assignment_pes, :ecosystem_matrix_uuid
+      add_index :assignment_spes, :calculation_uuid
+      add_index :assignment_spes, :ecosystem_matrix_uuid
+      add_index :student_pes, :calculation_uuid
+      add_index :student_pes, :ecosystem_matrix_uuid
+      add_index :student_clues, :calculation_uuid
+      add_index :teacher_clues, :calculation_uuid
 
-    add_column :student_pes, :ecosystem_matrix_uuid, :uuid
-    change_column_null :student_pes, :ecosystem_matrix_uuid, false, NIL_UUID
-    add_index :student_pes, :ecosystem_matrix_uuid
+      # Need to manually dump the schema or else it will become stale
+      Rake::Task['db:_dump'].invoke
+    end
 
-    add_column :student_clues, :calculation_uuid, :uuid
-    change_column_null :student_clues, :calculation_uuid, false, NIL_UUID
-    add_index :student_clues, :calculation_uuid
+    at_exit { background_migration.call }
+  end
 
-    add_column :teacher_clues, :calculation_uuid, :uuid
-    change_column_null :teacher_clues, :calculation_uuid, false, NIL_UUID
-    add_index :teacher_clues, :calculation_uuid
+  def down
+    ActiveRecord::Base.transaction do
+      remove_column :teacher_clues, :calculation_uuid, :uuid
+      remove_column :student_clues, :calculation_uuid, :uuid
+      remove_column :student_pes, :ecosystem_matrix_uuid, :uuid
+      remove_column :student_pes, :calculation_uuid, :uuid
+      remove_column :assignment_spes, :ecosystem_matrix_uuid, :uuid
+      remove_column :assignment_spes, :calculation_uuid, :uuid
+      remove_column :assignment_pes, :ecosystem_matrix_uuid, :uuid
+      remove_column :assignment_pes, :calculation_uuid, :uuid
+    end
   end
 end
