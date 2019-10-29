e509df0a1c11f3c2494197137a3ca02d2b135b1e
Dante Soares <dante.m.soares@rice.edu>
2019-10-29 10:29:44 -0500

    Also turns out change also works

--- db/migrate/20190930214433_add_calculation_uuid_and_ecosystem_matrix_uuid_to_exercises_and_clues.rb
+++ db/migrate/20190930214433_add_calculation_uuid_and_ecosystem_matrix_uuid_to_exercises_and_clues.rb
@@ -1,7 +1,7 @@
 class AddCalculationUuidAndEcosystemMatrixUuidToExercisesAndClues < ActiveRecord::Migration[5.2]
   NIL_UUID = '00000000-0000-0000-0000-000000000000'
 
-  def up
+  def change
     add_column :assignment_pes, :calculation_uuid, :uuid
     add_column :assignment_pes, :ecosystem_matrix_uuid, :uuid
     add_column :assignment_spes, :calculation_uuid, :uuid
@@ -11,45 +11,38 @@ class AddCalculationUuidAndEcosystemMatrixUuidToExercisesAndClues < ActiveRecord
     add_column :student_clues, :calculation_uuid, :uuid
     add_column :teacher_clues, :calculation_uuid, :uuid
 
-    # A simple background migration with the drawback that it will not retry errors
-    # This code will run after all migrations have finished
-    background_migration = -> do
-      # Become a daemon so the calling process can exit successfully
-      Process.daemon
+    reversible do |dir|
+      dir.up do
+        # A simple background migration with the drawback that it will not retry errors
+        # This code will run after all migrations have finished
+        background_migration = -> do
+          # Become a daemon so the calling process can exit successfully
+          Process.daemon
 
-      change_column_null :assignment_pes, :calculation_uuid, false, NIL_UUID
-      change_column_null :assignment_pes, :ecosystem_matrix_uuid, false, NIL_UUID
-      change_column_null :assignment_spes, :calculation_uuid, false, NIL_UUID
-      change_column_null :assignment_spes, :ecosystem_matrix_uuid, false, NIL_UUID
-      change_column_null :student_pes, :calculation_uuid, false, NIL_UUID
-      change_column_null :student_pes, :ecosystem_matrix_uuid, false, NIL_UUID
-      change_column_null :student_clues, :calculation_uuid, false, NIL_UUID
-      change_column_null :teacher_clues, :calculation_uuid, false, NIL_UUID
+          change_column_null :assignment_pes, :calculation_uuid, false, NIL_UUID
+          change_column_null :assignment_pes, :ecosystem_matrix_uuid, false, NIL_UUID
+          change_column_null :assignment_spes, :calculation_uuid, false, NIL_UUID
+          change_column_null :assignment_spes, :ecosystem_matrix_uuid, false, NIL_UUID
+          change_column_null :student_pes, :calculation_uuid, false, NIL_UUID
+          change_column_null :student_pes, :ecosystem_matrix_uuid, false, NIL_UUID
+          change_column_null :student_clues, :calculation_uuid, false, NIL_UUID
+          change_column_null :teacher_clues, :calculation_uuid, false, NIL_UUID
 
-      add_index :assignment_pes, :calculation_uuid
-      add_index :assignment_pes, :ecosystem_matrix_uuid
-      add_index :assignment_spes, :calculation_uuid
-      add_index :assignment_spes, :ecosystem_matrix_uuid
-      add_index :student_pes, :calculation_uuid
-      add_index :student_pes, :ecosystem_matrix_uuid
-      add_index :student_clues, :calculation_uuid
-      add_index :teacher_clues, :calculation_uuid
+          add_index :assignment_pes, :calculation_uuid
+          add_index :assignment_pes, :ecosystem_matrix_uuid
+          add_index :assignment_spes, :calculation_uuid
+          add_index :assignment_spes, :ecosystem_matrix_uuid
+          add_index :student_pes, :calculation_uuid
+          add_index :student_pes, :ecosystem_matrix_uuid
+          add_index :student_clues, :calculation_uuid
+          add_index :teacher_clues, :calculation_uuid
 
-      # Need to manually dump the schema or else it will become stale
-      Rake::Task['db:_dump'].invoke
+          # Need to manually dump the schema or else it will become stale
+          Rake::Task['db:_dump'].invoke
+        end
+
+        at_exit { background_migration.call }
+      end
     end
-
-    at_exit { background_migration.call }
-  end
-
-  def down
-    remove_column :teacher_clues, :calculation_uuid, :uuid
-    remove_column :student_clues, :calculation_uuid, :uuid
-    remove_column :student_pes, :ecosystem_matrix_uuid, :uuid
-    remove_column :student_pes, :calculation_uuid, :uuid
-    remove_column :assignment_spes, :ecosystem_matrix_uuid, :uuid
-    remove_column :assignment_spes, :calculation_uuid, :uuid
-    remove_column :assignment_pes, :ecosystem_matrix_uuid, :uuid
-    remove_column :assignment_pes, :calculation_uuid, :uuid
   end
 end
